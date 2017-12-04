c33cf37d20a7204808d460ac4e58eb205ee589c4
Naresh Miriyala <naresh.miriyala@redenergy.com.au>
2017-12-04 11:30:05 +1100

    First commit

---
+++ src/main/groovy/com/dellnaresh/MicroServices_Sample1/MicroServicesSample1Application.groovy
@@ -0,0 +1,12 @@
+package com.dellnaresh.MicroServices_Sample1
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MicroServicesSample1Application {
+
+	static void main(String[] args) {
+		SpringApplication.run MicroServicesSample1Application, args
+	}
+}
---
+++ src/test/groovy/com/dellnaresh/MicroServices_Sample1/MicroServicesSample1ApplicationTests.groovy
@@ -0,0 +1,16 @@
+package com.dellnaresh.MicroServices_Sample1
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class MicroServicesSample1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
